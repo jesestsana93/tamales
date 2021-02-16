b45e36951282bb7f598994caeb40772664031205
Jesus Sanchez <jesestsana93@hotmail.com>
2021-02-16 03:06:06 -0600

    seccion del menu terminada

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function () {
+    /*-----------------------------------------------------------
+        1. FUNCIONES PARA EL MENU PRINCIPAL
+    -------------------------------------------------------------*/
+    /*  Inserta y quita la clase ".icono-cerrar" al boton del menú*/
+    $('#menu-navegacion .navbar-toggler').click(function () {
+        $('.boton-menu').toggleClass('icono-cerrar');
+    });
+
+    /*Al hacer click en un enlace del menú principal */
+    $('#menu-navegacion .navbar-nav a').click(function () {
+        /* 1) Quita la clase ".icono-cerrar" */
+        $('.boton-menu').removeClass('icono-cerrar');
+
+        /*2) Contraemos el menu */
+        $('#menu-navegacion .navbar-collapse').collapse('hide');
+    });
+});
